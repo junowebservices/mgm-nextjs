8cdda6eb57970fac47ebb69ec5a94749e4c11d40
Jerome Escultura <jescultura@deloitte.com>
2023-07-07 12:54:05 +0800

    fixed date issues

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["images.unsplash.com"],
+    domains: ["images.pexels.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
