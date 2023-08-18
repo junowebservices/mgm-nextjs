985e78202137b91e28da4e504dc7a8c160766856
Jerome Escultura <jescultura@deloitte.com>
2023-08-18 21:40:41 +0800

    added calendar page

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,24 +1,17 @@
+import MonthCalendar from '@/components/MonthCalendar';
+import { Event } from '@/constants/types';
+
 const Calendar = () => {
-  return (
-    <div className="inside space-y-8 lg:w-7/12 my-24">
-      <h2 className="text-3xl">Calendar Coming Soon!</h2>
-      <p className="text-lg text-secondaryTextColor">
-        We are in the process of finalizing the calendar of events for National
-        Museum Month. Our team is working tirelessly to bring you an exceptional
-        array of experiences that showcase the best of our national museums.
-        Stay tuned as we update this page with the full calendar, complete with
-        dates, times, and details of each event. In the meantime, mark your
-        calendars and get ready for a month-long celebration of our cultural
-        treasures. Prepare to be inspired, educated, and entertained as we
-        embark on a journey through the captivating world of museums. For any
-        inquiries or to stay updated on National Museum Month's events, please
-        reach out to our dedicated team at mgmsecretariat@gmail.com. We
-        appreciate your patience and look forward to sharing an unforgettable
-        month with you! Note: The calendar of events will be available shortly.
-        Please check back soon for updates.
-      </p>
-    </div>
-  );
+	const events: Event[] = [
+		{ date: 5, title: 'Parade' },
+		{ date: 15, title: 'Event 2' },
+	];
+	return (
+		<div className='py-8 px-4'>
+			{/* <h1 className='text-xl font-semibold mb-4'>October 2023 Calendar</h1> */}
+			<MonthCalendar events={events} />
+		</div>
+	);
 };
 
 export default Calendar;
--- constants/types.tsx
+++ constants/types.tsx
@@ -1,22 +1,37 @@
 export interface Activity {
-  id: number;
-  title: string;
-  description: string;
-  date: string;
+	id: number;
+	title: string;
+	description: string;
+	date: string;
 }
 
 export interface PostMetadata {
-  title: string;
-  date: string;
-  subtitle: string;
-  slug: string;
-  featuredImage: string;
+	title: string;
+	date: string;
+	subtitle: string;
+	slug: string;
+	featuredImage: string;
 }
 
 export interface EventMetadata {
-  title: string;
-  date: string;
-  subtitle: string;
-  slug: string;
-  featuredImage: string;
+	title: string;
+	date: string;
+	subtitle: string;
+	slug: string;
+	featuredImage: string;
+}
+
+export interface Event {
+	date: number;
+	title: string;
+}
+
+export interface MonthCalendarProps {
+	events: Event[];
+}
+
+export interface CalendarDayProps {
+	day: number;
+	events: Event[];
+	hasEvents: boolean;
 }
